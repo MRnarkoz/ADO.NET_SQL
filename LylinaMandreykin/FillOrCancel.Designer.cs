7647df4d8daf1948450266277b09a65f97432ed9
MRnarkoz <mandreykinroman@gmail.com>
2021-04-21 11:52:09 +0400

    SQL1version

---
+++ LylinaMandreykin/FillOrCancel.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace LylinaMandreykin
+{
+    partial class FillOrCancel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFindByOrderID = new System.Windows.Forms.Button();
+            this.btnFinishUpdates = new System.Windows.Forms.Button();
+            this.btnFillOrder = new System.Windows.Forms.Button();
+            this.btnCancelOrder = new System.Windows.Forms.Button();
+            this.dtpFillDate = new System.Windows.Forms.DateTimePicker();
+            this.dgvCustomerOrders = new System.Windows.Forms.DataGridView();
+            this.txtOrderID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomerOrders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Идентификатор заказа:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(262, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "При заполнении заказа укажите дату заполнения";
+            // 
+            // btnFindByOrderID
+            // 
+            this.btnFindByOrderID.Location = new System.Drawing.Point(377, 7);
+            this.btnFindByOrderID.Name = "btnFindByOrderID";
+            this.btnFindByOrderID.Size = new System.Drawing.Size(97, 23);
+            this.btnFindByOrderID.TabIndex = 2;
+            this.btnFindByOrderID.Text = "Найти заказ";
+            this.btnFindByOrderID.UseVisualStyleBackColor = true;
+            this.btnFindByOrderID.Click += new System.EventHandler(this.btnFindByOrderID_Click);
+            // 
+            // btnFinishUpdates
+            // 
+            this.btnFinishUpdates.Location = new System.Drawing.Point(399, 232);
+            this.btnFinishUpdates.Name = "btnFinishUpdates";
+            this.btnFinishUpdates.Size = new System.Drawing.Size(75, 23);
+            this.btnFinishUpdates.TabIndex = 3;
+            this.btnFinishUpdates.Text = "Готово";
+            this.btnFinishUpdates.UseVisualStyleBackColor = true;
+            this.btnFinishUpdates.Click += new System.EventHandler(this.btnFinishUpdates_Click);
+            // 
+            // btnFillOrder
+            // 
+            this.btnFillOrder.Location = new System.Drawing.Point(120, 232);
+            this.btnFillOrder.Name = "btnFillOrder";
+            this.btnFillOrder.Size = new System.Drawing.Size(108, 23);
+            this.btnFillOrder.TabIndex = 4;
+            this.btnFillOrder.Text = "Заполнить заказ";
+            this.btnFillOrder.UseVisualStyleBackColor = true;
+            this.btnFillOrder.Click += new System.EventHandler(this.btnFillOrder_Click);
+            // 
+            // btnCancelOrder
+            // 
+            this.btnCancelOrder.Location = new System.Drawing.Point(12, 232);
+            this.btnCancelOrder.Name = "btnCancelOrder";
+            this.btnCancelOrder.Size = new System.Drawing.Size(102, 23);
+            this.btnCancelOrder.TabIndex = 5;
+            this.btnCancelOrder.Text = "Отменить заказ";
+            this.btnCancelOrder.UseVisualStyleBackColor = true;
+            this.btnCancelOrder.Click += new System.EventHandler(this.btnCancelOrder_Click);
+            // 
+            // dtpFillDate
+            // 
+            this.dtpFillDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFillDate.Location = new System.Drawing.Point(357, 109);
+            this.dtpFillDate.Name = "dtpFillDate";
+            this.dtpFillDate.Size = new System.Drawing.Size(117, 20);
+            this.dtpFillDate.TabIndex = 6;
+            this.dtpFillDate.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dgvCustomerOrders
+            // 
+            this.dgvCustomerOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomerOrders.Location = new System.Drawing.Point(12, 135);
+            this.dgvCustomerOrders.Name = "dgvCustomerOrders";
+            this.dgvCustomerOrders.ReadOnly = true;
+            this.dgvCustomerOrders.RowHeadersVisible = false;
+            this.dgvCustomerOrders.Size = new System.Drawing.Size(462, 91);
+            this.dgvCustomerOrders.TabIndex = 7;
+            // 
+            // txtOrderID
+            // 
+            this.txtOrderID.Location = new System.Drawing.Point(171, 10);
+            this.txtOrderID.Name = "txtOrderID";
+            this.txtOrderID.Size = new System.Drawing.Size(100, 20);
+            this.txtOrderID.TabIndex = 8;
+            // 
+            // FillOrCancel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 291);
+            this.Controls.Add(this.txtOrderID);
+            this.Controls.Add(this.dgvCustomerOrders);
+            this.Controls.Add(this.dtpFillDate);
+            this.Controls.Add(this.btnCancelOrder);
+            this.Controls.Add(this.btnFillOrder);
+            this.Controls.Add(this.btnFinishUpdates);
+            this.Controls.Add(this.btnFindByOrderID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FillOrCancel";
+            this.Text = "FillOrCancel";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomerOrders)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFindByOrderID;
+        private System.Windows.Forms.Button btnFinishUpdates;
+        private System.Windows.Forms.Button btnFillOrder;
+        private System.Windows.Forms.Button btnCancelOrder;
+        private System.Windows.Forms.DateTimePicker dtpFillDate;
+        private System.Windows.Forms.DataGridView dgvCustomerOrders;
+        private System.Windows.Forms.TextBox txtOrderID;
+    }
+}
---
+++ LylinaMandreykin/NewCustomer.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace LylinaMandreykin
+{
+    partial class NewCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.btnPlaceOrder = new System.Windows.Forms.Button();
+            this.btnAddAnotherAccount = new System.Windows.Forms.Button();
+            this.btnAddFinish = new System.Windows.Forms.Button();
+            this.numOrderAmount = new System.Windows.Forms.NumericUpDown();
+            this.dtpOrderDate = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.numOrderAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добавление учетной записи";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя заказчика:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Идентификатор клиента:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Создание заказа";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Сумма заказа:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Дата заказа:";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(133, 37);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(100, 20);
+            this.txtCustomerName.TabIndex = 6;
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(178, 62);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.ReadOnly = true;
+            this.txtCustomerID.Size = new System.Drawing.Size(54, 20);
+            this.txtCustomerID.TabIndex = 7;
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Location = new System.Drawing.Point(313, 62);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(150, 23);
+            this.btnCreateAccount.TabIndex = 8;
+            this.btnCreateAccount.Text = "Создать учетную запись";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            this.btnCreateAccount.Click += new System.EventHandler(this.btnCreateAccount_Click);
+            // 
+            // btnPlaceOrder
+            // 
+            this.btnPlaceOrder.Location = new System.Drawing.Point(339, 165);
+            this.btnPlaceOrder.Name = "btnPlaceOrder";
+            this.btnPlaceOrder.Size = new System.Drawing.Size(124, 23);
+            this.btnPlaceOrder.TabIndex = 9;
+            this.btnPlaceOrder.Text = "Разместить заказ";
+            this.btnPlaceOrder.UseVisualStyleBackColor = true;
+            this.btnPlaceOrder.Click += new System.EventHandler(this.btnPlaceOrder_Click);
+            // 
+            // btnAddAnotherAccount
+            // 
+            this.btnAddAnotherAccount.Location = new System.Drawing.Point(264, 234);
+            this.btnAddAnotherAccount.Name = "btnAddAnotherAccount";
+            this.btnAddAnotherAccount.Size = new System.Drawing.Size(199, 23);
+            this.btnAddAnotherAccount.TabIndex = 10;
+            this.btnAddAnotherAccount.Text = "Добавить другую учетную запись";
+            this.btnAddAnotherAccount.UseVisualStyleBackColor = true;
+            this.btnAddAnotherAccount.Click += new System.EventHandler(this.btnAddAnotherAccount_Click);
+            // 
+            // btnAddFinish
+            // 
+            this.btnAddFinish.Location = new System.Drawing.Point(73, 234);
+            this.btnAddFinish.Name = "btnAddFinish";
+            this.btnAddFinish.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFinish.TabIndex = 11;
+            this.btnAddFinish.Text = "Готово";
+            this.btnAddFinish.UseVisualStyleBackColor = true;
+            this.btnAddFinish.Click += new System.EventHandler(this.btnAddFinish_Click);
+            // 
+            // numOrderAmount
+            // 
+            this.numOrderAmount.Location = new System.Drawing.Point(133, 163);
+            this.numOrderAmount.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numOrderAmount.Name = "numOrderAmount";
+            this.numOrderAmount.Size = new System.Drawing.Size(100, 20);
+            this.numOrderAmount.TabIndex = 12;
+            this.numOrderAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // dtpOrderDate
+            // 
+            this.dtpOrderDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpOrderDate.Location = new System.Drawing.Point(133, 198);
+            this.dtpOrderDate.Name = "dtpOrderDate";
+            this.dtpOrderDate.Size = new System.Drawing.Size(100, 20);
+            this.dtpOrderDate.TabIndex = 13;
+            // 
+            // NewCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 283);
+            this.Controls.Add(this.dtpOrderDate);
+            this.Controls.Add(this.numOrderAmount);
+            this.Controls.Add(this.btnAddFinish);
+            this.Controls.Add(this.btnAddAnotherAccount);
+            this.Controls.Add(this.btnPlaceOrder);
+            this.Controls.Add(this.btnCreateAccount);
+            this.Controls.Add(this.txtCustomerID);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "NewCustomer";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.numOrderAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.Button btnCreateAccount;
+        private System.Windows.Forms.Button btnPlaceOrder;
+        private System.Windows.Forms.Button btnAddAnotherAccount;
+        private System.Windows.Forms.Button btnAddFinish;
+        private System.Windows.Forms.NumericUpDown numOrderAmount;
+        private System.Windows.Forms.DateTimePicker dtpOrderDate;
+    }
+}
